3f8465d30817fe9162a1a25875ff1774ece2185a
Marco <marcohdes94i@gmail.com>
2016-09-06 20:48:03 -0600

    Commit message

--- Sensor1.0/app/src/main/java/com/example/elrobotista/sensor10/MainActivity.java
+++ Sensor1.0/app/src/main/java/com/example/elrobotista/sensor10/MainActivity.java
@@ -13,6 +13,8 @@ import android.widget.TextView;
 
 import org.json.JSONArray;
 
+import java.io.IOException;
+import java.io.InputStream;
 import java.io.OutputStream;
 import java.io.OutputStreamWriter;
 import java.io.PrintStream;
@@ -28,10 +30,11 @@ public class MainActivity extends Activity implements SensorEventListener {
     private TextView texto1;
     private TextView texto2;
     private Socket socket;
-    //private float[] arraySensor = new float[3];
+    private float[] arraySensor = new float[3];
     private String socketOutput;
     private OutputStream outStream;
-    private PrintWriter out;
+    private PrintStream out;
+    private Boolean connected;
 
 
     public MainActivity() {
@@ -43,12 +46,13 @@ public class MainActivity extends Activity implements SensorEventListener {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        connected = false;
         mSensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
         mAccelerometer = mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
         texto = (TextView) findViewById(R.id.textView);
         texto1 = (TextView) findViewById(R.id.textView2);
         texto2 = (TextView) findViewById(R.id.textView3);
-        //mSensorManager.registerListener(this, mAccelerometer, SensorManager.SENSOR_DELAY_NORMAL);
+        mSensorManager.registerListener(this, mAccelerometer, SensorManager.SENSOR_DELAY_NORMAL);
 
 
 
@@ -58,19 +62,19 @@ public class MainActivity extends Activity implements SensorEventListener {
 
     class SocketThread implements Runnable {
 
+
         public void run() {
 
             try {
-                socket = new Socket("192.168.43.181", 5001);
-                out = new PrintWriter(new OutputStreamWriter(socket.getOutputStream()), true);
-                //outStream = new PrintStream(socket.getOutputStream());
-               // out= new PrintWriter(outStream);
-                outStream = socket.getOutputStream();
-                out = new PrintWriter(outStream);
-                out.print(socketOutput);
 
+                socket = new Socket("192.168.0.6", 5001);
+                //out = new PrintWriter(new OutputStreamWriter(socket.getOutputStream()), true);
+                out = new PrintStream(socket.getOutputStream(),true);
+                //outStream = socket.getOutputStream();
+                //new PrintWriter(out);
+                //out.println(outStream);
 
-
+                connected = true;
 
             } catch (java.io.IOException e) {
                 e.printStackTrace();
@@ -103,18 +107,24 @@ public class MainActivity extends Activity implements SensorEventListener {
         texto1.setText(Float.toString(event.values[1]));
         texto2.setText(Float.toString(event.values[2]));/* cambio Float.toString por String.valueO */
 
+        if(connected == true) {
 
-        socketOutput= Float.toString(event.values[0]);
+           // socketOutput = Float.toString(event.values[0]);
+            //new  PrintWriter (out);
+            //out.println(socketOutput);
+            //out.flush();
+            // new PrintWriter(out);
+            String s = String.format("{\"x\": %f, \"y\": %f, \"z\": %f}", event.values[0], event.values[1], event.values[2]);
+            out.print(s);
 
-        
+            //out.flush();
+        }
 
+        // Agregar un boton  para conectarse y desconectarse
+        // text input de la direcciona conectar y puerto
+        // alinear valores
 
-
-       // String s = String.format("{\"x\": %f}",event.values[0]);
-        //out.print(s);
-
-        //out.flush();
-
+        // 1# conectarse basico en python server
 
         //como imprimir strings en el socket.
 
